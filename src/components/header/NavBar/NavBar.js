727ed02f4549b58ce692e0a407cec93a9923ad9c
Andrei Felecan <andreifcn@gmail.com>
2021-05-10 13:54:41 +0300

    fixes

--- src/components/header/NavBar/NavBar.js
+++ src/components/header/NavBar/NavBar.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+
+  const refreshSiteData = () => {
+
+    localStorage.clear('persist:persistedStore');
+    
+    return window.location = '/';
+  }
   
     return (
       <nav className='nav-bar'>
         <Link to='/'>
-          <p id='index-link'>CineView</p>
+          <p id='index-link' onClick={refreshSiteData}>CineView</p>
         </Link>
         <Link to='/favourites'>
           <p className='link'>about</p>

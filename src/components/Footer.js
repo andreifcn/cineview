8c9bcd29e4357e2e2fd1c65b87c555fdedd6bff1
Andrei Felecan <andreifcn@gmail.com>
2021-05-12 10:34:55 +0300

    fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
 
     return <div className='footer'>
         <p id='to-top-page' onClick={() => window.scrollTo(0,0)}>{`^Top`}</p>
-        <a id='tvmaze-link' href='https://www.tvmaze.com/api' target='_blank'>Powered by TVMAZE API</a>
+        <a id='tvmaze-link' href='https://www.tvmaze.com/api' target='_blank' without rel='noreferrer'>Powered by TVMAZE API</a>
         <p id='site-owner' style={{color: 'aliceblue'}}>Andrei Felecan</p>
     </div>
 }
